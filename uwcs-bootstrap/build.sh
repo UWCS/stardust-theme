4dfed45db749812babe2cfb634b26396a320c917
ericthelemur <owenconnors1@gmail.com>
2023-09-11 23:36:47 +0100

    Make variables partial and make build.sh default to grass

--- uwcs-bootstrap/build.sh
+++ uwcs-bootstrap/build.sh
@@ -1,15 +1,21 @@
-if ! which sass
+if which grass
 then
-    if [ ! -f "dart-sass/sass" ]
-    then
-        echo 'No SASS present, installing SASS from https://github.com/sass/dart-sass'
-        wget 'https://github.com/sass/dart-sass/releases/download/1.66.1/dart-sass-1.66.1-linux-x64.tar.gz'
-        tar -xf dart-sass-1.66.1-linux-x64.tar.gz
-        rm dart-sass-1.66.1-linux-x64.tar.gz
-    fi
-    cmd="./dart-sass/sass"
+    cmd="grass"
 else
-    cmd="sass"
-fi
+    echo 'Install grass for maximum compatability with Zola (but nearly equiv to sass) `cargo install grass`'
+    if which sass
+    then
+        cmd="sass"
+    else
+        if [ ! -f "dart-sass/sass" ]
+        then
+            echo 'No SASS present, installing SASS from https://github.com/sass/dart-sass'
+            wget 'https://github.com/sass/dart-sass/releases/download/1.66.1/dart-sass-1.66.1-linux-x64.tar.gz'
+            tar -xf dart-sass-1.66.1-linux-x64.tar.gz
+            rm dart-sass-1.66.1-linux-x64.tar.gz
+        fi
+        cmd="./dart-sass/sass"
+    fi
+
 
 "$cmd" uwcs.scss ../static/uwcs-bootstrap.css --style compressed
