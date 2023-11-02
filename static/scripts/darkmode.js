6412a4b4f55f4bd98aa2981ab31201183aa7c78b
ericthelemur <owenconnors1@gmail.com>
2023-11-02 23:07:49 +0000

    Move initial dark mode set to inline

--- static/scripts/darkmode.js
+++ static/scripts/darkmode.js
@@ -1,10 +1,3 @@
-const dark = function () {
-    const curr_light = localStorage.getItem("dark_mode") == "light";
-    document.documentElement.setAttribute('data-bs-theme', curr_light ? "light" : "dark");
-}
-
-dark();
-
 // Set toggle button on load
 window.addEventListener('load', function () {
     document.getElementById('dark-mode').addEventListener('click', () => {
